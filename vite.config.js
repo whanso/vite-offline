74144eee2199b3b0a46b9fe0a269fc76f00507b1
ctwh <39967134+ctwh@users.noreply.github.com>
2023-01-20 21:23:03 -0500

    Initial commit.

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      mode: "development",
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.js",
+      injectManifest: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+        type: "module",
+        navigateFallbackAllowlist: [/^index.html$/],
+      },
+    }),
+  ],
+});
